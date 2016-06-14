b2e0b677f1000bf0557961be93a941c58d36393d
Richard Cartwright <spark@streampunk.media>
2016-06-14 18:36:41 +0100

    Added puppeteer - a means to pull all the metadata sets together into a single structured preface.

---
+++ pipelines/puppeteer.js
@@ -0,0 +1,102 @@
+/* Copyright 2016 Streampunk Media Ltd.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+var H = require('highland');
+var meta = require('../util/meta.js');
+
+function puppeteer() {
+  var cache = { };
+  var preface = null;
+  var headerByteCount = 0;
+  var headerEnd = -1;
+  function resolveAll(item) {
+    // console.log('Resolving', item.ObjectClass);
+    Object.keys(item).forEach(function (name) {
+      if (name === 'InstanceUID' || name === 'PrimaryPackage') return;
+      var value = item[name];
+      if (typeof value === 'string' && value.match(
+          /[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}/)) {
+        var sub = cache[value];
+        if (sub) {
+          resolveAll(sub);
+          item[name] = sub;
+        };
+      } else if (Array.isArray(value)) {
+        var subArray = [];
+        value.forEach(function (subElement) {
+          if (typeof subElement === 'string' && subElement.match(
+              /[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}/)) {
+            var sub = cache[subElement];
+            if (sub) {
+              resolveAll(sub);
+              subArray.push(sub);
+            } else {
+              subArray.push(subElement);
+            };
+          } else {
+            subArray.push(subElement);
+          };
+        });
+        item[name] = subArray;
+      };
+    });
+  };
+
+  var pullTheStrings = function (err, x, push, next) {
+    if (err) {
+      push(err);
+      next();
+    } else if (x === H.nil) {
+      if (preface) push(null, preface); // Push partially constructed preface
+      push(null, H.nil);
+    } else {
+      if (x.meta.Symbol.endsWith('PartitionPack') &&
+          x.detail.HeaderByteCount > 0) {
+        headerByteCount = x.detail.HeaderByteCount;
+        headerEnd = -1;
+        preface = null;
+        cache = { };
+        push(null, x);
+        return next();
+      };
+      if (x.meta.Symbol === 'PrimerPack') { // Beginning of header metaata
+        headerEnd = x.filePos + headerByteCount;
+        return next();// Don't send the primer pack on - used locally only
+      }
+      if (headerEnd < 0) { // Not collecting header classes
+        push(null, x);
+        return next();
+      }
+      if (x.meta.Symbol === 'Preface') {
+        preface = x.detail;
+      };
+      if (x.filePos >= headerEnd) { // We're done ... stop resolving
+        resolveAll(preface);
+        push(null, preface);
+        push(null, x);
+        headerEnd = -1;
+        headerByteCount = 0;
+        preface = null;
+        cache = {};
+        return next();
+      }
+      if (x.detail.InstanceUID) cache[x.detail.InstanceUID] = x.detail;
+      next();
+    }
+  };
+  return H.pipeline(H.consume(pullTheStrings));
+};
+
+module.exports = puppeteer;
