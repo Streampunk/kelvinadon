225d917ea7b8d16d43ad9c84d5123bd2e5cefbd1
Richard Cartwright <spark@streampunk.media>
2016-07-08 11:05:43 +0100

    Added smoke test of streaming MXF over FTP using a UK DPP file.

---
+++ scratch/smokeFTP.js
@@ -0,0 +1,51 @@
+/* Copyright 2016 Streampunk Media Ltd.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+var H = require('highland');
+var fs = require('fs');
+var klv = require('../index.js');
+var FTP = require('ftp');
+
+var c = new FTP();
+c.on('ready', function () {
+  var get = c.get(
+      'dppdownload/dpp_example_files/AS11_DPP_HD_EXAMPLE_1.mxf',
+      function (err, stream) {
+    if (err) return console.error(err);
+    var base = H(stream)
+    .through(klv.kelviniser())
+    .through(klv.metatiser())
+    .through(klv.stripTheFiller)
+    .through(klv.detailing())
+    .through(klv.puppeteer())
+    .through(klv.trackCacher());
+
+    base.fork()
+     .through(klv.essenceFilter('picture0'))
+     .doto(H.log)
+     .errors(function (e) { console.error(e); })
+     .done(function () { console.log('Finished reading picture data.'); });
+
+    base.fork()
+      .through(klv.essenceFilter('sound0'))
+      .doto(H.log)
+      .errors(function (e) { console.error(e); })
+      .done(function () { console.log('Finished reading sound data.') });
+    base.resume();
+
+    stream.on('close', function () { console.log("End of the stream.")});
+  });
+});
+
+c.connect({ host : "ftp.kw.bbc.co.uk"})
